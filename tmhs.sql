7be3e3af45aefc12f724815df93c132e23eb9881
RifkyF <rifky14f@gmail.com>
2022-02-22 13:48:18 +0700

    full coding

---
+++ tmhs.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 21, 2022 at 11:09 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dblatihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmhs`
+--
+
+CREATE TABLE `tmhs` (
+  `id_mhs` int(11) NOT NULL,
+  `nim` varchar(9) CHARACTER SET latin1 NOT NULL,
+  `nama` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `alamat` varchar(150) CHARACTER SET latin1 NOT NULL,
+  `prodi` varchar(80) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tmhs`
+--
+ALTER TABLE `tmhs`
+  ADD PRIMARY KEY (`id_mhs`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tmhs`
+--
+ALTER TABLE `tmhs`
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1310;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
